f5af79fdc0aee24c468e03dfeeb8bc1b83148089
Brandon Johnson <brandon.johnson@space150.com>
2017-06-30 21:20:38 -0500

    SQUASH lol well i guess im committing this

---
+++ src/ui/components/work-category/component.ts
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class WorkEntry extends Component {
+  entries = [
+    {
+      "title": "waterselfie",
+      "desc": "a selfie thru water",
+      "url": "https://www.space150.com/work/pentair/"
+    }
+  ]
+
+};
