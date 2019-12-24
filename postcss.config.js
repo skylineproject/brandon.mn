411d0ee84535b3968607a3b19dc20e7dcf013997
Brandon Johnson <b@johnsonmn.org>
2019-12-23 21:46:32 -0600

    Remove PostCSS extractor for the time being

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
     plugins: [
       require('postcss-easy-import'),
       require('tailwindcss')('./tailwind.config.js'),
-      isProd ? require('@fullhuman/postcss-purgecss')({
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }) : undefined,
       require('autoprefixer'),
       require('cssnano'),
     ],
